8a944c5204c0ebcae3b22c623b4014562e21c3c8
Ryan Meyer <rmmeyer19@gmail.com>
2024-01-31 11:31:06 -0700

    Set up basic routing for multiple modals in extension

--- editorextensions/UVexplorer-integration/src/extension.ts
+++ editorextensions/UVexplorer-integration/src/extension.ts
@@ -11,6 +11,28 @@ class HelloWorldModal extends Modal {
     }
 }
 
+class FirstModal extends Modal {
+    constructor(client: EditorClient) {
+        super(client, {
+            title: 'First',
+            width: 800,
+            height: 600,
+            url: 'http://localhost:4200/first'
+        });
+    }
+}
+
+class SecondModal extends Modal {
+    constructor(client: EditorClient) {
+        super(client, {
+            title: 'Second',
+            width: 800,
+            height: 600,
+            url: 'http://localhost:4200/second'
+        });
+    }
+}
+
 const client = new EditorClient();
 const menu = new Menu(client);
 
@@ -23,4 +45,26 @@ client.registerAction('hello', () => {
 menu.addDropdownMenuItem({
     label: 'Say Hello',
     action: 'hello'
+});
+
+client.registerAction('first', () => {
+    const modal = new FirstModal(client);
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    modal.show();
+});
+
+menu.addDropdownMenuItem({
+    label: 'First',
+    action: 'first'
+});
+
+client.registerAction('second', () => {
+    const modal = new SecondModal(client);
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    modal.show();
+});
+
+menu.addDropdownMenuItem({
+    label: 'Second',
+    action: 'second'
 });
