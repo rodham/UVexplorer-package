2b4fa78dd5ca7d051121866b39b28c58f6039bf1
Ethan Borsky <e99train@gmail.com>
2024-02-08 17:03:56 -0700

    Abstracted out common uvx client logic

--- editorextensions/UVexplorer-integration/src/uvexplorer-modal.ts
+++ editorextensions/UVexplorer-integration/src/uvexplorer-modal.ts
@@ -1,71 +1,20 @@
-import { DataSourceProxy, EditorClient, JsonSerializable, Modal, Viewport } from 'lucid-extension-sdk';
-import { UVExplorerClient } from './uvexplorer-client';
+import { DataSourceProxy, EditorClient, JsonSerializable, Viewport } from 'lucid-extension-sdk';
 import { isLoadNetworkMessage } from '../model/message';
 import { DeviceListRequest, NetworkRequest } from '../model/uvexplorer-model';
+import { UVXModal } from './uvx-modal';
 import {
     addDevicesToCollection,
     createOrRetrieveDeviceCollection,
     createOrRetrieveNetworkSource
 } from './data-collections';
 
-export class UVexplorerModal extends Modal {
+export class UVexplorerModal extends UVXModal {
     private viewport: Viewport;
-    private uvexplorerClient: UVExplorerClient;
-    private serverUrl = '';
-    private apiKey = '';
-    private sessionGuid = '';
 
     constructor(client: EditorClient, viewport: Viewport) {
-        super(client, {
-            title: 'UVexplorer',
-            width: 800,
-            height: 600,
-            url: 'http://localhost:4200/networks'
-        });
+        super(client);
 
         this.viewport = viewport;
-        this.uvexplorerClient = new UVExplorerClient(client);
-    }
-
-    public async configureSetting(setting: string) {
-        let settings = await this.client.getPackageSettings();
-        if (!settings.get(setting)) {
-            if (await this.client.canEditPackageSettings()) {
-                await this.client.alert(
-                    `You have not configured the ${setting}. You will now be prompted to complete that configuration.`
-                );
-                await this.client.showPackageSettingsModal();
-                settings = await this.client.getPackageSettings();
-                if (!settings.get(setting)) {
-                    return;
-                }
-            } else {
-                await this.client.alert(
-                    `Your account has not configured the ${setting}. Talk with your Lucid account administrator to complete configuration.`
-                );
-            }
-        }
-    }
-
-    public async loadSettings() {
-        const settings = await this.client.getPackageSettings();
-        const apiKey = settings.get('apiKey');
-        const serverUrl = settings.get('serverUrl');
-
-        if (typeof apiKey == 'string' && typeof serverUrl == 'string') {
-            this.apiKey = apiKey;
-            this.serverUrl = serverUrl;
-        }
-    }
-
-    async openSession() {
-        this.sessionGuid = await this.uvexplorerClient.openSession(this.serverUrl, this.apiKey);
-        console.log(`Successfully opened a session: ${this.sessionGuid}`);
-    }
-
-    async closeSession() {
-        await this.uvexplorerClient.closeSession(this.serverUrl, this.sessionGuid);
-        console.log(`Successfully closed the session.`);
     }
 
     async listNetworks() {
---
+++ editorextensions/UVexplorer-integration/src/uvx-modal.ts
@@ -0,0 +1,41 @@
+import { EditorClient, Modal, Viewport } from 'lucid-extension-sdk';
+import { UVExplorerClient } from './uvexplorer-client';
+
+export abstract class UVXModal extends Modal {
+    protected uvexplorerClient: UVExplorerClient;
+    protected serverUrl = '';
+    protected apiKey = '';
+    protected sessionGuid = '';
+
+    constructor(client: EditorClient) {
+        super(client, {
+            title: 'UVexplorer',
+            width: 800,
+            height: 600,
+            url: 'http://localhost:4200/networks'
+        });
+
+        this.uvexplorerClient = new UVExplorerClient(client);
+    }
+
+    public async loadSettings() {
+        const settings = await this.client.getPackageSettings();
+        const apiKey = settings.get('apiKey');
+        const serverUrl = settings.get('serverUrl');
+
+        if (typeof apiKey == 'string' && typeof serverUrl == 'string') {
+            this.apiKey = apiKey;
+            this.serverUrl = serverUrl;
+        }
+    }
+
+    async openSession() {
+        this.sessionGuid = await this.uvexplorerClient.openSession(this.serverUrl, this.apiKey);
+        console.log(`Successfully opened a session: ${this.sessionGuid}`);
+    }
+
+    async closeSession() {
+        await this.uvexplorerClient.closeSession(this.serverUrl, this.sessionGuid);
+        console.log(`Successfully closed the session.`);
+    }
+}
