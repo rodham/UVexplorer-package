3f12b05e0f1e3fd2cf8f69aa601747f910f1683f
Ryan Meyer <rmmeyer19@gmail.com>
2024-02-09 10:28:43 -0700

    Reorganize custom shape code

--- editorextensions/UVexplorer-integration/src/network-devices/network-device-blocks.ts
+++ editorextensions/UVexplorer-integration/src/network-devices/network-device-blocks.ts
@@ -1,17 +1,17 @@
 import { EditorClient } from 'lucid-extension-sdk';
-import { Device } from '../model/uvexplorer-model';
+import { Device } from '../../model/uvexplorer-model';
 import { NetworkDeviceBlockModel } from './network-device-block-model';
 
 export class NetworkDeviceBlocks {
     private client: EditorClient;
-    private lastId = 0;
+    private nextId = 0;
 
     constructor(client: EditorClient) {
         this.client = client;
     }
 
     private incrementId() {
-        this.lastId++;
+        this.nextId++;
     }
 
     private findCategory(deviceTypes: Set<string>) {
@@ -46,25 +46,34 @@ export class NetworkDeviceBlocks {
         return "unknown-device";
     }
 
+    private getCompany(device: Device) {
+        const info_sets = JSON.parse(JSON.stringify(device.info_sets));
+        let company = "unknown-make";
+        if (info_sets.product_info != undefined) {
+            company = info_sets.product_info.vendor;
+        }
+        return company;
+    }
+
+    private getDeviceType(device: Device) {
+        const deviceTypes: Set<string> = new Set();
+        device.device_categories.entries.forEach(type => {
+            deviceTypes.add(type.device_category);
+        });
+
+        const deviceType = this.findCategory(deviceTypes);
+        return deviceType;
+    }
+
     public convertDevicesToBlocks(devices: Device[]) {
         let deviceBlocks: NetworkDeviceBlockModel[] = [];
 
         for(const device of devices) {
-            const info_sets = JSON.parse(JSON.stringify(device.info_sets));
-            let company = "unknown-make";
-            if (info_sets.product_info != undefined) {
-                company = info_sets.product_info.vendor;
-            }
+            const company = this.getCompany(device);
+            const deviceType = this.getDeviceType(device);
 
-            const deviceTypes: Set<string> = new Set();
-            device["device_categories"]["entries"].forEach(type => {
-                deviceTypes.add(type["device_category"]);
-            });
-
-            const deviceType = this.findCategory(deviceTypes);
-
+            let newBlock = new NetworkDeviceBlockModel(this.nextId as any as string, this.client, company, deviceType);
             this.incrementId();
-            let newBlock = new NetworkDeviceBlockModel(this.lastId as any as string, this.client, company, deviceType);
             deviceBlocks.push(newBlock);
         }
         
