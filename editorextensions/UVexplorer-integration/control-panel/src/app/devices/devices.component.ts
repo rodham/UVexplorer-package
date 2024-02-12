954a2c82f5c84870049b7d3ea59b0c1d8ce81070
Russell Dupaix <rjdupaix@gmail.com>
2024-02-11 21:19:57 -0700

    Created a device selection list

--- editorextensions/UVexplorer-integration/control-panel/src/app/devices/devices.component.ts
+++ editorextensions/UVexplorer-integration/control-panel/src/app/devices/devices.component.ts
@@ -1,18 +1,22 @@
 import { Component } from '@angular/core';
 import { NgIf, NgFor } from '@angular/common';
-import { isListDevicesMessage } from 'model/message';
-import { Device } from 'model/uvexplorer-model';
+import { isListDevicesMessage } from '../../../../model/message';
+import { Device, DeviceCategories } from '../../../../model/uvexplorer-model';
+import { AgGridAngular } from 'ag-grid-angular';
+import { ColDef, RowSelectedEvent, ICellRenderer } from 'ag-grid-community';
 
 @Component({
   selector: 'app-devices',
   standalone: true,
-  imports: [NgIf, NgFor],
+  imports: [NgIf, NgFor, AgGridAngular],
   templateUrl: './devices.component.html',
   styleUrl: './devices.component.css'
 })
 export class DevicesComponent {
   devices: Device[] = [];
   selectedDevices: Device[] = [];
+  themeClass = 'ag-theme-quartz';
+  rowSelection: 'multiple' | 'single' = 'multiple';
 
   constructor() {
     window.addEventListener('message', (e) => {
@@ -26,23 +30,47 @@ export class DevicesComponent {
     });
   }
 
-  protected toggleDeviceSelect(device: Device) {
-    if (this.selectedDevices.includes(device)) {
-      const index = this.selectedDevices.findIndex((obj) => obj === device);
+  public columnDefs: ColDef<Device>[] = [
+    {
+      field: 'ip_address',
+      headerName: 'IP Address',
+      headerCheckboxSelection: true,
+      checkboxSelection: true,
+      filter: 'agTextColumnFilter',
+    },
+    {
+      field: 'mac_address',
+      headerName: 'MAC Address',
+      filter: 'agTextColumnFilter',
+    },
+    {
+      field: 'custom_name',
+      headerName: 'Custom Name',
+      filter: 'agTextColumnFilter',
+    },
+    {
+      field: 'timestamp',
+      headerName: 'Timestamp',
+      filter: 'agTextColumnFilter',
+    }
+  ];
+
+  protected onRowSelected(event: RowSelectedEvent) {
+    if (event.node.isSelected() && event.node.data instanceof Device) {
+      this.selectedDevices.push(event.node.data);
+    } else {
+      const index = this.selectedDevices.findIndex((obj) => obj === event.node.data);
       this.selectedDevices.splice(index, 1);
-      return;
     }
 
-    this.selectedDevices.push(device);
+    console.log("Selected length ", this.selectedDevices.length);
   }
 
   protected selectDevices() {
     parent.postMessage(
-      {
-        action: 'selectDevices',
-        devices: JSON.stringify(this.selectedDevices)
-      },
-      '*'
-    );
+    {
+      action: 'selectDevices',
+      devices: JSON.stringify(this.selectedDevices)
+    }, '*');
   }
 }
