afddaaeb010797a08823bf93e33e06a8d4b350ac
e99train <e99train@users.noreply.github.com>
2024-03-06 17:06:35 +0000

    Apply prettier formatting changes

--- editorextensions/UVexplorer-integration/control-panel/src/app/AppComponent/app.component.spec.ts
+++ editorextensions/UVexplorer-integration/control-panel/src/app/AppComponent/app.component.spec.ts
@@ -2,23 +2,23 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppComponent]
-    }).compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [AppComponent]
+        }).compileComponents();
+    });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    it('should create the app', () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.componentInstance;
 
-    expect(app).toBeTruthy();
-  });
+        expect(app).toBeTruthy();
+    });
 
-  it(`should have the 'control-panel' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    it(`should have the 'control-panel' title`, () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.componentInstance;
 
-    expect(app.title).toEqual('control-panel');
-  });
+        expect(app.title).toEqual('control-panel');
+    });
 });
--- editorextensions/UVexplorer-integration/control-panel/src/app/app.routes.ts
+++ editorextensions/UVexplorer-integration/control-panel/src/app/app.routes.ts
@@ -5,20 +5,20 @@ import { DeviceDetailComponent } from './device-details/device-detail.component'
 import { LinkDetailComponent } from './link-details/link-detail.component';
 
 export const routes: Routes = [
-  {
-    path: 'networks',
-    component: NetworksComponent
-  },
-  {
-    path: 'devices',
-    component: DevicesComponent
-  },
-  {
-    path: 'device-detail',
-    component: DeviceDetailComponent
-  },
-  {
-    path: 'link-detail',
-    component: LinkDetailComponent
-  }
+    {
+        path: 'networks',
+        component: NetworksComponent
+    },
+    {
+        path: 'devices',
+        component: DevicesComponent
+    },
+    {
+        path: 'device-detail',
+        component: DeviceDetailComponent
+    },
+    {
+        path: 'link-detail',
+        component: LinkDetailComponent
+    }
 ];
