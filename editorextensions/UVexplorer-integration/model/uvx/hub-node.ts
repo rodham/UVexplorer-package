760a27b0c8e8f4657874e5c68fd081c5829b9f91
jameskrueger00 <79768190+jameskrueger00@users.noreply.github.com>
2024-03-13 10:18:18 -0600

    feat: add type guard for hub nodes

--- editorextensions/UVexplorer-integration/model/uvx/hub-node.ts
+++ editorextensions/UVexplorer-integration/model/uvx/hub-node.ts
@@ -10,6 +10,32 @@
     type: MultiNodeType;
 }
 
+export function isHubNode(obj: unknown): obj is HubNode {
+    return (
+        typeof obj === 'object' &&
+        obj !== null &&
+        'nodeId' in obj &&
+        typeof obj.nodeId === 'number' &&
+        'label' in obj &&
+        typeof obj.label === 'string' &&
+        'x' in obj &&
+        typeof obj.x === 'number' &&
+        'y' in obj &&
+        typeof obj.y === 'number' &&
+        'centerX' in obj &&
+        typeof obj.centerX === 'number' &&
+        'bottom' in obj &&
+        typeof obj.bottom === 'number' &&
+        'width' in obj &&
+        typeof obj.width === 'number' &&
+        'height' in obj &&
+        typeof obj.height === 'number' &&
+        'type' in obj &&
+        typeof obj.type === 'number' &&
+        Object.values(MultiNodeType).includes(obj.type)
+    );
+}
+
 export enum MultiNodeType {
     Hub,
     WirelessClient,
--- editorextensions/UVexplorer-integration/model/uvx/topo-map.ts
+++ editorextensions/UVexplorer-integration/model/uvx/topo-map.ts
@@ -266,6 +266,7 @@ export interface TopoMap {
     displayEdges?: unknown;
 }
 
+//TODO this type guard can be stricter once we are in a more final state
 export function isTopoMap(obj: unknown): obj is TopoMap {
     return (
         typeof obj === 'object' &&
@@ -281,7 +282,7 @@ export function isTopoMap(obj: unknown): obj is TopoMap {
         'hubNodes' in obj &&
         Array.isArray(obj.hubNodes) &&
         'imageNodes' in obj &&
-        Array.isArray(obj.imageNodes) && // Adjust the typing once known
+        Array.isArray(obj.imageNodes) &&
         'deviceLinks' in obj &&
         Array.isArray(obj.deviceLinks) &&
         'width' in obj &&
