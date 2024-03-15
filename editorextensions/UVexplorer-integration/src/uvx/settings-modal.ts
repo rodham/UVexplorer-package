f55e1857c5eacd6dd3dd0e65114fef895fe0402e
Ryan Meyer <rmmeyer19@gmail.com>
2024-03-14 21:11:59 -0600

    Add redraw map functionality when changing settings

--- editorextensions/UVexplorer-integration/src/uvx/settings-modal.ts
+++ editorextensions/UVexplorer-integration/src/uvx/settings-modal.ts
@@ -4,17 +4,30 @@ import { isSelectedMapSettingsMessage } from 'model/message';
 import { DocumentClient } from 'src/doc/document-client';
 import { UVExplorerClient } from '@uvx/uvx-client';
 import { DataClient } from '@data/data-client';
+import { NetworkRequest } from 'model/uvx/network';
 
 export class SettingsModal extends UVXModal {
     constructor(client: EditorClient, docClient: DocumentClient, uvxClient: UVExplorerClient, data: DataClient) {
         super(client, docClient, uvxClient, data, 'settings');
     }
 
-    protected messageFromFrame(message: JsonSerializable) {
+    async redrawMap() {
+        let devices: string[] = this.docClient.getNetworkDeviceBlockGuids();
+        if (devices.length > 1) {
+            // TODO: I need to load the correct network before I redraw the map - just need to test this
+            const networkGuid: string = this.dataClient.getNetworkForPage(this.docClient.getPageId()!);
+            await this.uvxClient.loadNetwork(new NetworkRequest(networkGuid));
+            await this.drawMap(devices, []);
+        }
+    }
+
+    protected async messageFromFrame(message: JsonSerializable) {
         console.log('Received a message from the child.');
         console.log(message);
         if (isSelectedMapSettingsMessage(message)) {
             this.docClient.saveSettings(message.drawSettings, message.layoutSettings);
+            await this.redrawMap();
+            await this.closeSession();
             this.hide();
         }
     }
