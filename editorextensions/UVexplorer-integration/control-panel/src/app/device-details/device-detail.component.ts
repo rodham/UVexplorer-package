9ecb7f74c0d4a3554ce1134e9264966bb5f21cb4
jameskrueger00 <79768190+jameskrueger00@users.noreply.github.com>
2024-03-29 01:32:58 -0600

    feat: redo ui for details modals

--- editorextensions/UVexplorer-integration/control-panel/src/app/device-details/device-detail.component.ts
+++ editorextensions/UVexplorer-integration/control-panel/src/app/device-details/device-detail.component.ts
@@ -2,16 +2,18 @@ import { Component } from '@angular/core';
 import { NgIf, NgFor } from '@angular/common';
 import { Device, DeviceDetailsResponse } from 'model/uvx/device';
 import { deviceDetailsFromMessage, deviceFromSerializableDeviceMessage, isDeviceDetailsMessage } from 'model/message';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'device-detail',
     standalone: true,
-    imports: [NgIf, NgFor],
+    imports: [NgIf, NgFor, FormsModule],
     templateUrl: './device-detail.component.html'
 })
 export class DeviceDetailComponent {
     device?: Device;
     deviceDetails?: DeviceDetailsResponse;
+    selectedTab = 'System';
 
     constructor() {
         window.addEventListener('message', (e) => {
